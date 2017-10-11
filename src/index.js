eabad8ad81fb9806b9fcd5b2ad7dd14b0262dfe5
Joey Smoll <jsmoll91@gmail.com>
2017-10-11 14:07:10 -0700

    my language complete

---
+++ src/index.js
@@ -0,0 +1,22 @@
+var Alexa = require('alexa-sdk');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function() {
+        this.emit('LaunchIntent');
+    },
+
+    'LaunchIntent': function() {
+        this.emit(':ask', "Hi, what is your language?");
+    },
+
+    'LanguageIntent': function() {
+        var myLanguage = this.event.request.intent.slots.myLanguage.value;
+        this.emit(':tell', "Your language is  " + myLanguage);
+    }
+};
